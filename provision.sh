cae53a590cb98619536f9fd1023ccf5ec00ecc2b
xybersolve <xybersolve@gmail.com>
2016-06-14 17:55:52 -0700

    update provision.sh to use our nginx configuration

--- provision.sh
+++ provision.sh
@@ -4,10 +4,18 @@ apt-get -y update
 
 apt-get -y install nginx
 
-cp -r /usr/share/nginx/www /vagrant/www
+# delete nginx config file 
+rm -rf /etc/nginx/site-enabled
+# copy in our own config file
+cp -r /vagrant/sites-enabled /etc/nginx
 
+# only used in etup of provisioning
+#cp -r /usr/share/nginx/www /vagrant/www
+
+# delete nginx document root 
 rm -rf /usr/share/nginx/www
-
+# and create symlink for document root to our project files
 ln -s /vagrant/www /usr/share/nginx/www
 
+# finally start nginx
 service nginx start
