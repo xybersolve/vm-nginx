6c829f813b3cd9f53f982f7ad01d46d901c9cb1b
xybersolve <xybersolve@gmail.com>
2016-06-14 17:15:23 -0700

    symlink nginx to shared /vagrant/www directory after moving web docs there

--- provision.sh
+++ provision.sh
@@ -4,4 +4,10 @@ apt-get -y update
 
 apt-get -y install nginx
 
+cp -r /usr/share/nginx/www /vagrant/www
+
+rm -rf /usr/share/nginx/www
+
+ln -s /vagrant/www /usr/share/nginx/www
+
 service nginx start
